d38670d1018e5cb58fa2092e8ffd6be82ca4a772
Garrett <39309647+Gle123@users.noreply.github.com>
2021-11-18 10:18:11 -0600

    Add files via upload

---
+++ deleteOldTabPyFiles.ps1
@@ -0,0 +1,9 @@
+#script that deletes tabpy logs
+$dateSaveRange = (Get-Date).AddDays(-10)            #10 days
+$transferDirectory = "\\vuhl\bu\Tableau\Tabpy Logs"
+
+#deletes files older than x days
+ECHO ".....DELETING OLD LOGS....."
+Get-ChildItem $transferDirectory -file *TabPyLogs -Recurse | Where-Object {($_.LastWriteTime -lt $dateSaveRange)} | Remove-Item
+
+Write-Host 'Files cleared and deleted'
