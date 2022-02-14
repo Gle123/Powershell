7633f835f8733bc52f65fea00283b788bc8d4d96
Garrett <39309647+Gle123@users.noreply.github.com>
2022-02-14 16:40:39 -0600

    Add files via upload

---
+++ ServerCheck.ps1
@@ -0,0 +1,19 @@
+#Checks server status. If server down restart server and send email. If server up ignore script
+
+#If Tableau Server is not RUNNING, send an email reading Restart Failed and print Server not running
+$Status = (tsm status).ToString()
+
+Write-Output "`n`r$($Status)"    
+if ($Status -ne 'Status: RUNNING'){
+$lookupSMTP = Resolve-DnsName smtp.veteransunited.com
+$smtpaddr = [string]$lookupSMTP.NameHost
+#Send email
+Send-MailMessage -SmtpServer $smtpaddr -Port 25 -From "PCWTableauWarning@veteransunited.com" -To "garrett.le@veteransunited.com","andrea.hardy@veteransunited.com" -Subject "Server Restart Failed" -Body "Tableau Server is down. Restarting server. Please check status." -UseSsl 
+Write-Output "Server is not running. Email sent.`n`r" 
+tsm stop
+tsm start
+
+#If Tableau Server is RUNNING, ignore script
+}else {
+Write-Output "`n`r Yay, Server is running.`n`r"  
+}
