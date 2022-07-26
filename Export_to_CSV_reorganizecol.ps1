1f735798ffe13efad95efaaa9f7faa9afb7fd3cd
Garrett <39309647+Gle123@users.noreply.github.com>
2022-07-26 10:28:09 -0500

    Create Export_to_CSV_reorganizecol.ps1

---
+++ Export_to_CSV_reorganizecol.ps1
@@ -0,0 +1,41 @@
+#*****CHANGE LOG*************************************************************************
+
+#*****End Change Log*********************************************************************
+
+$tableauinstalldirectory = "E:\Tableau Server\packages\bin.20213.22.0108.1051\"
+$filename1 = "\\name\of\directory"+".csv"
+$filename2 = "\\name\of\directory"+".csv"
+$localfilename1 = "\\name\of\directory"+".csv"
+$localfilename2 = "\\name\of\directory"+".csv"
+
+#navigate to Tableau Bin directory
+cd $tableauinstalldirectory
+
+#Deleting the old files from the directory so that only the newest file is there
+ECHO "-------------Removing old files------------"
+Remove-Item -path "\\name\of\directory*.csv*"
+ECHO "-------------Old files removed-directory empty--------"
+
+#Starting a tabcmd session - logging into the server
+ECHO "=====Starting a Tabcmd session and logging into the server"
+.\tabcmd login -s https://url.com -u username -p password --no-certcheck
+
+#Exporting specified workbook views to .csv file
+ECHO "=====Exporting view to .csv file"
+.\tabcmd.exe get "views/#.csv" --filename $localfilename1 --no-certcheck
+.\tabcmd.exe get "views/#.csv" --filename $localfilename2 --no-certcheck
+
+#Importing/reading the new file name, sorting by column in descending order, then exporting the new sorted file to .CSV 
+ECHO "-------------IMPORTING FILE-SORTING by COLUMN-EXPORTING NEW SORTED CSV FILE-------------"
+Import-Csv $localfilename1 | SELECT 'Google Click ID', 'Conversion Name', 'Conversion Time', 'Conversion Value', 'Currency'  | Export-Csv -path $filename1 -NoTypeInformation 
+Import-Csv $localfilename2 | SELECT 'Google Click ID', 'Conversion Name', 'Conversion Time', 'Conversion Value', 'Currency'  | Export-Csv -path $filename2 -NoTypeInformation 
+
+#Removing .csv from local location
+Remove-Item -Path $localfilename1
+Remove-Item -Path $localfilename2
+ 
+#End tabcmd sesson - logging out
+ECHO "=====Complete. Sessing ending"
+.\tabcmd logout
+
+exit
